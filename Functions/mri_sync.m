ec0684f7f68da45df8d4b782c269114b058f3377
alboryck <alborcyk@utexas.edu>
2016-07-19 15:20:35 -0500

    ADDED: MRI sync now displays message

--- Functions/mri_sync.m
+++ Functions/mri_sync.m
@@ -1,4 +1,6 @@
-function time = mri_sync()
-    KbWait()
+function time = mri_sync(Display)
+    DrawFormattedText(Display.window,'We are going to show you pictures of food. \n\n Press the joystick trigger to continue.','center','center',[255 255 255],50,[],[],1.5);
+    Screen('Flip', Display.window);
+    KbWait();
     time = GetSecs();
 end
